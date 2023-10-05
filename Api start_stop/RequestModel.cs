a75f99d81b58077c04a17bb1ddc723896ef2a18f
Supriya <supriya@ensarsolutions.com>
2023-10-05 15:58:46 +0530

    Initial commit

---
+++ Api start_stop/RequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Apistart_stop
+{
+    public class RequestModel
+    {
+         public string AWSAccessKey { get; set; }
+         public string AWSSecretKey { get; set; }
+         public string Region { get; set; }
+
+        
+    }
+}
